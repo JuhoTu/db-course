e552ceb7996e7439b37c87412ade6a4ef1984f55
JuhoTu <54879860+JuhoTu@users.noreply.github.com>
2020-04-11 13:17:19 +0300

    added files

---
+++ db-task/db-task/Services/PersonService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace db_task.Services
+{
+    class PersonService
+    {
+    }
+}
---
+++ db-task/db-task/Views/IPersonView.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace db_task.Views
+{
+    interface IPersonView
+    {
+    }
+}
